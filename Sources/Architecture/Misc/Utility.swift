c8c34dce8088e187526531db5207ca5fa422d342
Jackson Utsch <jutechs@gmail.com>
2021-05-03 21:18:32 -0700

    hex Color

--- Sources/Architecture/Misc/Utility.swift
+++ Sources/Architecture/Misc/Utility.swift
@@ -79,3 +79,16 @@ public enum Constructor {
     case `default`
     case preview
 }
+
+// MARK: Color hex
+extension Color {
+    public init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
