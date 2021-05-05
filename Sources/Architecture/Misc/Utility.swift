b9d63d488253976a9e40930ded01a5a83cb9b3db
Jackson Utsch <jutechs@gmail.com>
2021-05-05 09:51:22 -0700

    spacing

--- Sources/Architecture/Misc/Utility.swift
+++ Sources/Architecture/Misc/Utility.swift
@@ -10,87 +10,87 @@ import SwiftUI
 
 // MARK: Optional Contains
 public extension Optional where Wrapped: Collection, Wrapped.Element: Equatable {
-    func contains(_ item: Wrapped.Iterator.Element) -> Bool {
-        if let array = self {
-            if array.contains(item) {
-                return true
-            }
-        }
-        return false
+  func contains(_ item: Wrapped.Iterator.Element) -> Bool {
+    if let array = self {
+      if array.contains(item) {
+        return true
+      }
     }
+    return false
+  }
 }
 
 // MARK: Optional Binding
 public func ??<T>(lhs: Binding<Optional<T>>, rhs: T) -> Binding<T> {
-    Binding(
-        get: { lhs.wrappedValue ?? rhs },
-        set: { lhs.wrappedValue = $0 }
-    )
+  Binding(
+    get: { lhs.wrappedValue ?? rhs },
+    set: { lhs.wrappedValue = $0 }
+  )
 }
 
 // MARK: Optional Subscripting
 public extension Collection {
-    /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    subscript(safe index: Index?) -> Element? {
-        guard let index = index else {
-            return nil
-        }
-        return indices.contains(index) ? self[index] : nil
+  /// Returns the element at the specified index if it is within bounds, otherwise nil.
+  subscript(safe index: Index?) -> Element? {
+    guard let index = index else {
+      return nil
     }
+    return indices.contains(index) ? self[index] : nil
+  }
 }
 
 public extension Array where Element: Identifiable {
-    /// Returns the first element with the specefied id, otherwise nil.
-    subscript(safe id: Element.ID?) -> Element? {
-        guard let id = id else {
-            return nil
-        }
-        return self.filter({$0.id == id}).first
+  /// Returns the first element with the specefied id, otherwise nil.
+  subscript(safe id: Element.ID?) -> Element? {
+    guard let id = id else {
+      return nil
     }
+    return self.filter({$0.id == id}).first
+  }
 }
 
 // MARK: Frame Shorthand
 public extension View {
-    func width(_ value: CGFloat) -> some View {
-        self
-            .frame(width: value)
-    }
-    
-    func height(_ value: CGFloat) -> some View {
-        self
-            .frame(height: value)
-    }
+  func width(_ value: CGFloat) -> some View {
+    self
+      .frame(width: value)
+  }
+  
+  func height(_ value: CGFloat) -> some View {
+    self
+      .frame(height: value)
+  }
 }
 
 // MARK: CGSize
 public extension CGSize {
-    var short: CGFloat {
-        return Swift.min(self.width, self.height)
-    }
-    
-    var long: CGFloat {
-        return Swift.max(self.width, self.height)
-    }
+  var short: CGFloat {
+    return Swift.min(self.width, self.height)
+  }
+  
+  var long: CGFloat {
+    return Swift.max(self.width, self.height)
+  }
 }
 
 // MARK: Constructor Enum
 public enum Constructor {
-    case empty
-    case `default`
-    case preview
+  case empty
+  case `default`
+  case preview
 }
 
 // MARK: Color hex
 extension Color {
-    public init(hex: UInt, alpha: Double = 1) {
-        self.init(
-            .sRGB,
-            red: Double((hex >> 16) & 0xff) / 255,
-            green: Double((hex >> 08) & 0xff) / 255,
-            blue: Double((hex >> 00) & 0xff) / 255,
-            opacity: alpha
-        )
-    }
+  public init(hex: UInt, alpha: Double = 1) {
+    self.init(
+      .sRGB,
+      red: Double((hex >> 16) & 0xff) / 255,
+      green: Double((hex >> 08) & 0xff) / 255,
+      blue: Double((hex >> 00) & 0xff) / 255,
+      opacity: alpha
+    )
+  }
 }
 
 // MARK: setMap
--- Sources/Architecture/Store.swift
+++ Sources/Architecture/Store.swift
@@ -14,182 +14,186 @@ import CombineSchedulers
 // MARK: Store
 @available(iOS 13, macOS 10.15, *)
 public class Store<State: Equatable, Action, Environment>: ObservableObject {
-    @UniquePublished public private(set) var state: State
-    private let environment: Environment
-    private let reducer: (inout State, Action, Environment) -> AnyPublisher<Action, Never>?
-    internal let scheduler: AnySchedulerOf<DispatchQueue>
-    
-    private var cancellables: [AnyCancellable] = []
-    
-    private var actionsDebug: Log.Level? = nil
-    private var stateChangeDebug: Log.Level? = nil
-    
-    public init(
-        initialState: State,
-        reducer: @escaping (inout State, Action, Environment) -> AnyPublisher<Action, Never>?,
-        environment: Environment,
-        scheduler: AnySchedulerOf<DispatchQueue> = .main
-    ) {
-        self.state = initialState
-        self.reducer = reducer
-        self.environment = environment
-        self.scheduler = scheduler
+  @UniquePublished public private(set) var state: State
+  private let environment: Environment
+  private let reducer: (inout State, Action, Environment) -> AnyPublisher<Action, Never>?
+  internal let scheduler: AnySchedulerOf<DispatchQueue>
+  
+  private var cancellables: [AnyCancellable] = []
+  
+  private var actionsDebug: Log.Level? = nil
+  private var stateChangeDebug: Log.Level? = nil
+  
+  public init(
+    initialState: State,
+    reducer: @escaping (inout State, Action, Environment) -> AnyPublisher<Action, Never>?,
+    environment: Environment,
+    scheduler: AnySchedulerOf<DispatchQueue> = .main
+  ) {
+    self.state = initialState
+    self.reducer = reducer
+    self.environment = environment
+    self.scheduler = scheduler
+  }
+  
+  private convenience init?(
+    initialState: State?,
+    reducer: @escaping (inout State, Action, Environment) -> AnyPublisher<Action, Never>?,
+    environment: Environment
+  ) {
+    guard let initialState = initialState else {
+      return nil
+    }
+    self.init(initialState: initialState, reducer: reducer, environment: environment)
+  }
+  
+  public func send(_ action: Action) {
+    if let level = actionsDebug {
+      DispatchQueue.global(qos: .utility).async {
+        Log.custom(level: level, message: action)
+      }
     }
     
-    private convenience init?(
-        initialState: State?,
-        reducer: @escaping (inout State, Action, Environment) -> AnyPublisher<Action, Never>?,
-        environment: Environment
-    ) {
-        guard let initialState = initialState else {
-            return nil
+    let tempState = state
+    if let effect = reducer(&state, action, environment) {
+      effect.receive(on: scheduler)
+        .sink { [unowned self] result in
+          self.send(result)
+        }.store(in: &cancellables)
+    }
+    
+    if let level = stateChangeDebug {
+      DispatchQueue.global(qos: .utility).async { [unowned self] in
+        let diff = dumpDiff(state, tempState).joined()
+        if diff.count > 0 {
+          Log.custom(level: level, message: "\n" + "\(self.self) \n" + diff + "\n")
         }
-        self.init(initialState: initialState, reducer: reducer, environment: environment)
+      }
     }
-    
-    public func send(_ action: Action) {
-        if let level = actionsDebug {
-            DispatchQueue.global(qos: .utility).async {
-                Log.custom(level: level, message: action)
-            }
-        }
-        
-        let tempState = state
-        if let effect = reducer(&state, action, environment) {
-            effect.receive(on: scheduler)
-                .sink { [unowned self] result in
-                    self.send(result)
-                }.store(in: &cancellables)
-        }
-        
-        if let level = stateChangeDebug {
-            DispatchQueue.global(qos: .utility).async { [unowned self] in
-                let diff = dumpDiff(state, tempState).joined()
-                if diff.count > 0 {
-                    Log.custom(level: level, message: "\n" + "\(self.self) \n" + diff + "\n")
-                }
-            }
-        }
-    }
-    
-    /// Establishes logging preferences
-    public func debug(actions: Log.Level? = nil, stateChanges: Log.Level? = nil) {
-        self.actionsDebug = actions
-        self.stateChangeDebug = stateChanges
-    }
-        
-    /// Callback for local state on state changes including declaration
-    public func observe<LocalState>(
-        get: @escaping (State) -> LocalState,
-        callback: @escaping (LocalState) -> ()
-    ) {
-        $state.map { get($0) }
-            .sink { callback($0) }
-            .store(in: &cancellables)
-    }
+  }
+  
+  /// Establishes logging preferences
+  public func debug(
+    actions: Log.Level? = nil,
+    stateChanges: Log.Level? = nil
+  ) {
+    self.actionsDebug = actions
+    self.stateChangeDebug = stateChanges
+  }
+  
+  /// Callback for local state on state changes including declaration
+  public func observe<LocalState>(
+    get: @escaping (State) -> LocalState,
+    callback: @escaping (LocalState) -> ()
+  ) {
+    $state.map { get($0) }
+      .sink { callback($0) }
+      .store(in: &cancellables)
+  }
 }
 
 // MARK: Substores
 extension Store {
-    /// Scoped store that observes and sends changes to the parent
-    public func substore<LocalState, LocalAction, LocalEnvironment>(
-        state toLocalState: @escaping (State) -> LocalState,
-        action fromLocalAction: @escaping (LocalAction) -> Action,
-        env toLocalEnvironment: @escaping (Environment) -> LocalEnvironment
-    ) -> Store<LocalState, LocalAction, LocalEnvironment> {
-        let localStore = Store<LocalState, LocalAction, LocalEnvironment>(
-            initialState: toLocalState(self.state),
-            reducer: { localState, localAction, localEnvironment in
-                self.send(fromLocalAction(localAction))
-                return nil
-            }, environment: toLocalEnvironment(environment))
-        
-        self.$state.receive(on: scheduler)
-            .sink { [weak localStore] newState in
-                localStore?.state = toLocalState(newState)
-            }.store(in: &cancellables)
-        return localStore
-    }
-
-    /**
-     Scoped store that observes and sends changes to the parent
-     
-     Used for destructive components such as nested state
-    */
-    public func subscriptStore<LocalState, LocalAction, LocalEnvironment>(
-        state toLocalState: @escaping (State) -> LocalState?,
-        action fromLocalAction: @escaping (LocalAction) -> Action,
-        env toLocalEnvironment: @escaping (Environment) -> LocalEnvironment
-    ) -> Store<LocalState, LocalAction, LocalEnvironment>? {
-        if let localStore = Store<LocalState, LocalAction, LocalEnvironment>(
-            initialState: toLocalState(self.state),
-            reducer: { localState, localAction, localEnvironment in
-                self.send(fromLocalAction(localAction))
-                return nil
-            }, environment: toLocalEnvironment(environment)) {
-
-            self.$state.receive(on: scheduler)
-                .sink { [weak localStore] newState in
-                    if let newLocalState = toLocalState(newState) {
-                        localStore?.state = newLocalState
-                    }
-                }.store(in: &cancellables)
-            return localStore
-        }
+  /// Scoped store that observes and sends changes to the parent
+  public func substore<LocalState, LocalAction, LocalEnvironment>(
+    state toLocalState: @escaping (State) -> LocalState,
+    action fromLocalAction: @escaping (LocalAction) -> Action,
+    env toLocalEnvironment: @escaping (Environment) -> LocalEnvironment
+  ) -> Store<LocalState, LocalAction, LocalEnvironment> {
+    let localStore = Store<LocalState, LocalAction, LocalEnvironment>(
+      initialState: toLocalState(self.state),
+      reducer: { localState, localAction, localEnvironment in
+        self.send(fromLocalAction(localAction))
         return nil
+      }, environment: toLocalEnvironment(environment)
+    )
+    
+    self.$state.receive(on: scheduler)
+      .sink { [weak localStore] newState in
+        localStore?.state = toLocalState(newState)
+      }.store(in: &cancellables)
+    return localStore
+  }
+  
+  /**
+   Scoped store that observes and sends changes to the parent
+   
+   Used for destructive components such as nested state
+   */
+  public func subscriptStore<LocalState, LocalAction, LocalEnvironment>(
+    state toLocalState: @escaping (State) -> LocalState?,
+    action fromLocalAction: @escaping (LocalAction) -> Action,
+    env toLocalEnvironment: @escaping (Environment) -> LocalEnvironment
+  ) -> Store<LocalState, LocalAction, LocalEnvironment>? {
+    if let localStore = Store<LocalState, LocalAction, LocalEnvironment>(
+        initialState: toLocalState(self.state),
+        reducer: { localState, localAction, localEnvironment in
+          self.send(fromLocalAction(localAction))
+          return nil
+        }, environment: toLocalEnvironment(environment)) {
+      
+      self.$state.receive(on: scheduler)
+        .sink { [weak localStore] newState in
+          if let newLocalState = toLocalState(newState) {
+            localStore?.state = newLocalState
+          }
+        }.store(in: &cancellables)
+      return localStore
     }
+    return nil
+  }
 }
 
 // MARK: Bindings
 extension Store {
-    /// SwiftUI Binding with action
-    @available(iOS 14, macOS 11.0, *)
-    public func binding<LocalState>(
-        get: @escaping (State) -> LocalState,
-        send localState: @escaping (LocalState) -> Action
-        ) -> Binding<LocalState> {
-        let binding = Binding(
-            get: { get(self.state) },
-            set: { newLocalState, transaction in
-                if transaction.animation != nil {
-                  withTransaction(transaction) {
-                    self.send(localState(newLocalState))
-                  }
-                } else {
-                    self.send(localState(newLocalState))
-                }
-            }
-        )
-        return binding
-    }
-    
-    /// SwiftUI Binding with callback
-    @available(iOS 14, macOS 11.0, *)
-    public func binding<LocalState>(
-        get: @escaping (State) -> LocalState,
-        callback: @escaping (LocalState) -> ()
-        ) -> Binding<LocalState> {
-        Binding(
-            get: { get(self.state) },
-            set: { newLocalState in
-                callback(newLocalState)
-            }
-        )
-    }
+  /// SwiftUI Binding with action
+  @available(iOS 14, macOS 11.0, *)
+  public func binding<LocalState>(
+    get: @escaping (State) -> LocalState,
+    send localState: @escaping (LocalState) -> Action
+  ) -> Binding<LocalState> {
+    let binding = Binding(
+      get: { get(self.state) },
+      set: { newLocalState, transaction in
+        if transaction.animation != nil {
+          withTransaction(transaction) {
+            self.send(localState(newLocalState))
+          }
+        } else {
+          self.send(localState(newLocalState))
+        }
+      }
+    )
+    return binding
+  }
+  
+  /// SwiftUI Binding with callback
+  @available(iOS 14, macOS 11.0, *)
+  public func binding<LocalState>(
+    get: @escaping (State) -> LocalState,
+    callback: @escaping (LocalState) -> ()
+  ) -> Binding<LocalState> {
+    Binding(
+      get: { get(self.state) },
+      set: { newLocalState in
+        callback(newLocalState)
+      }
+    )
+  }
 }
 
 public typealias StoreBinding<S: Equatable> = Store<S, S, Void>
 
 extension StoreBinding {
-    public convenience init(constant: State) {
-        self.init(initialState: constant, reducer: {_,_,_ in nil}, environment: () as! Environment)
-    }
+  public convenience init(constant: State) {
+    self.init(initialState: constant, reducer: {_,_,_ in nil}, environment: () as! Environment)
+  }
 }
 
 extension Store {
-    public func storeBinding<LocalState: Equatable>(
-        get: @escaping (State) -> LocalState,
-        set: @escaping (LocalState) -> Action) -> StoreBinding<LocalState> {
-        return substore(state: get, action: set, env: {_ in})
-    }
+  public func storeBinding<LocalState: Equatable>(
+    get: @escaping (State) -> LocalState,
+    set: @escaping (LocalState) -> Action) -> StoreBinding<LocalState> {
+    return substore(state: get, action: set, env: {_ in})
+  }
 }
